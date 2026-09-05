6bd76dd55b05713c3fdd6cb4ad155a55ddc509b0
taloriko <67206692+taloriko@users.noreply.github.com>
2026-09-05 21:57:59 +0200

    Zuordnungsstatus und Version testen

--- tests/validate_structure.py
+++ tests/validate_structure.py
@@ -17,6 +17,7 @@ with (ROOT / 'EVTile' / 'locale.json').open(encoding='utf-8') as f:
 assert GUID.match(library['id'])
 assert GUID.match(module['id'])
 assert library['compatibility']['version'] >= '8.1'
+assert library['version'] == '1.1'
 assert module['type'] == 3
 assert module['vendor'] == 'taloriko'
 assert module['prefix'].isalnum()
@@ -31,6 +32,11 @@ assert 'GetVisualizationTile' in php
 assert 'UpdateVisualizationValue' in php
 assert 'RegisterMessage($id, VM_UPDATE)' in php
 assert 'RequestAction($id, $value)' in php
+assert 'GetConfigurationForm' in php
+assert 'assignmentStatus' in php
+assert 'ROLE_NAME_ALIASES' in php
+assert 'buildState()' in php
+assert '$ids = $this->resolveVariables();' in php
 assert 'ApiKey' not in html
 assert 'RequestsRemaining' not in html
 assert 'PartialErrors' not in html

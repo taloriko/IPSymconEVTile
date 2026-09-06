e60f1fd35929e1412e38b2d4c3a2576c949beb90
taloriko <67206692+taloriko@users.noreply.github.com>
2026-09-06 19:57:12 +0200

    Replace HTML visualization with native objects

--- EVTile/module.php
+++ EVTile/module.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 require_once __DIR__ . '/src/CoreTrait.php';
 require_once __DIR__ . '/src/MappingTrait.php';
 require_once __DIR__ . '/src/ObjectTreeTrait.php';
-require_once __DIR__ . '/src/VisualizationTrait.php';
+require_once __DIR__ . '/src/ChartTrait.php';
 
 final class EVTile extends IPSModuleStrict
 {
     use EVTileCoreTrait;
     use EVTileMappingTrait;
     use EVTileObjectTreeTrait;
-    use EVTileVisualizationTrait;
+    use EVTileChartTrait;
 }
